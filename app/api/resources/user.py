9942530c03df5e125fbad8c1bc3244ea339d4243
Ritika5901234 <ritika590@gmail.com>
2020-05-24 18:31:11 +0530

    isssue 562 -fix

--- app/api/resources/user.py
+++ app/api/resources/user.py
@@ -252,10 +252,11 @@ class UserRegister(Resource):
     @users_ns.response(HTTPStatus.OK, "%s" % messages.USER_WAS_CREATED_SUCCESSFULLY)
     @users_ns.response(
         HTTPStatus.BAD_REQUEST,
-        "%s\n%s"
+        "%s\n%s\n%s"
         % (
             messages.USER_USES_A_USERNAME_THAT_ALREADY_EXISTS,
             messages.USER_USES_AN_EMAIL_ID_THAT_ALREADY_EXISTS,
+            messages.EMAIL_INPUT_BY_USER_IS_INVALID
         ),
     )
     @users_ns.expect(register_user_api_model, validate=True)
